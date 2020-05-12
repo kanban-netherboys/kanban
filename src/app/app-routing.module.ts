905a21c62b142f25f2cb84b3724cec03c58e4ec6
Oskar <oskarbrudnowski@gmail.com>
2020-05-12 15:19:55 +0200

    old versions deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { CoreZeroComponent } from './layout/core-zero/core-zero.component';
-import { CoreOneComponent } from './layout/core-one/core-one.component';
-import { TransitionComponent } from './layout/core-one/transition/transition.component';
 import { CoreOneOldComponent } from './shared/components/core-one-old/core-one-old.component';
 import { CoreTwoModule } from './layout/core-two/core-two.module';
 import { CoreTwoComponent } from './layout/core-two/core-two.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './layout/layout.component';
-import { CoreZeroModule } from './layout/core-zero/core-zero.module';
-import { CoreOneModule } from './layout/core-one/core-one.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreOneOldModule } from './shared/components/core-one-old/core-one-old.module';
@@ -23,8 +21,6 @@ import { CoreTwoModule } from './layout/core-two/core-two.module';
       AppRoutingModule,
       BrowserAnimationsModule,
       SharedModule,
-      CoreZeroModule,
-      CoreOneModule,
       CoreOneOldModule,
       CoreTwoModule
    ],
